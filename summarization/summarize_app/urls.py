85b16f225fefab4fedf5ed883e7f24f22b0d3e49
daryabaldina1702 <ddbaldina@mail.ru>
2024-04-07 01:38:48 +0300

    Доотправление последних изменений

--- summarization/summarize_app/urls.py
+++ summarization/summarize_app/urls.py
@@ -1,16 +1,23 @@
 from django.urls import path
-from .views import check_description, DescriptionList, description_detail, DescriptionCreate, get_and_save_description_by_id,\
-UpdateDescription, DeleteDescription, summarize_text, RatingCreateView, program_editors
+from .views import  DescriptionList, description_detail, DescriptionCreate, get_and_save_description_by_id,\
+UpdateDescription, DeleteDescription, summarize_text, create_rating, program_editors, main_page, SummaryList, RatingList, DeleteSummary,\
+UpdateSummary
+#check_description
 
 urlpatterns = [
-    path('main/', check_description),
-    path('descriptions/', DescriptionList.as_view()),
-    path('description/<int:pk>', description_detail),
-    path('create_description/',  DescriptionCreate.as_view()),
-    path('API/description/',  get_and_save_description_by_id),
-    path('description/update/<int:pk>', UpdateDescription.as_view()),
-    path("description/delete/<int:pk>", DeleteDescription.as_view()),
-    path('summarize/',  summarize_text),
-    path('rating/', RatingCreateView.as_view()),
-    path('description/editors/<int:work_program_id>', program_editors),
+    path('main/', main_page, name = 'main_page'),
+    # path('main/', check_description),
+    path('descriptions/', DescriptionList.as_view(), name = 'description'),
+    path('description/<int:pk>', description_detail, name = 'info'),
+    path('create_description/',  DescriptionCreate.as_view(), name = 'create_description'),
+    path('API/description/',  get_and_save_description_by_id, name = 'get_description'),
+    path('description/update/<int:pk>', UpdateDescription.as_view(), name = 'update_description'),
+    path("description/delete/<int:pk>", DeleteDescription.as_view(), name = 'delete_description'),
+    path('summarize/<int:work_program_id>',  summarize_text, name = 'summarization'),
+    path('summarize/', SummaryList.as_view(), name = 'summary'),
+    path('summarize/update/<int:pk>', UpdateSummary.as_view(), name = 'update_summary'),
+    path("summarize/delete/<int:pk>", DeleteSummary.as_view(), name = 'delete_summary'),
+    path('rating/<int:summarization_id>', create_rating, name = 'rating'),
+    path('rating/', RatingList.as_view(), name= 'all_rating'),
+    path('description/editors/<int:work_program_id>', program_editors, name = 'editors'),
 ]
--- summarization/summarize_app/views.py
+++ summarization/summarize_app/views.py
@@ -1,30 +1,34 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Description, Summary_text, Rating, Editor
 from django.http import HttpResponse
-from .forms import ProgramIdForm, SummarizationForm
+from .forms import  SummarizationForm, RatingForm #ProgramIdForm
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 import requests
 import json
 from transformers import AutoTokenizer, T5ForConditionalGeneration
+from django.shortcuts import redirect
 
 
+#главная страница
+def main_page(request):
+    return render(request, 'main_page.html')
 
-# функция, которая проверяет, что описание есть в БД
-def check_description(request):
-    description_exists = None
-    if request.method == 'POST':
-        form = ProgramIdForm(request.POST)
-        if form.is_valid():
-            work_program_id = form.cleaned_data['work_program_id']
-            try:
-                program = Description.objects.get(pk=work_program_id)
-                if hasattr(program, 'description_text'):
-                    description_exists = True
-            except Description.DoesNotExist:
-                    description_exists = False
-    else:
-        form = ProgramIdForm()
-    return render(request, 'description_check.html', {'form': form, 'description_exists': description_exists})
+# # функция, которая проверяет, что описание есть в БД
+# def check_description(request):
+#     description_exists = None
+#     if request.method == 'POST':
+#         form = ProgramIdForm(request.POST)
+#         if form.is_valid():
+#             work_program_id = form.cleaned_data['work_program_id']
+#             try:
+#                 program = Description.objects.get(pk=work_program_id)
+#                 if hasattr(program, 'description_text'):
+#                     description_exists = True
+#             except Description.DoesNotExist:
+#                     description_exists = False
+#     else:
+#         form = ProgramIdForm()
+#     return render(request, 'description_check.html', {'form': form, 'description_exists': description_exists})
 
 # функция, которая выводит все описания
 class DescriptionList(ListView):
@@ -43,9 +47,9 @@ def description_detail(request, pk):
 # функция, которая добавляет описание
 class DescriptionCreate(CreateView):
     model = Description
-    fields = ['work_program_id', 'description_text']  # Укажите здесь все поля, которые должны быть в форме
+    fields = ['work_program_id', 'description_text']
     template_name='create_description.html'
-    success_url = '/main'
+    success_url = '/descriptions/'
 
 #функция, которая отдает пользователю описание по ID, а если его нет в БД, то подтягивает описание через API конструктора
 def get_and_save_description_by_id(request):
@@ -57,6 +61,7 @@ def get_and_save_description_by_id(request):
         try:
             description = Description.objects.get(work_program_id=description_id)
             description_text = description.description_text
+            return redirect(f'/description/{description_id}')
         except Description.DoesNotExist:
             api_url = 'https://op.itmo.ru/api/workprogram/detail/'
             url = f'{api_url}{description_id}'
@@ -93,16 +98,17 @@ class UpdateDescription(UpdateView):
     model = Description
     fields = ['description_text']
     template_name = 'update_description.html'
-    success_url = '/main'
+    success_url = '/descriptions/'
 
 # удаляет описание по id
 class DeleteDescription(DeleteView):
     model = Description
     template_name = 'del_description.html'
-    success_url = '/main'
+    success_url = '/descriptions/'
+
 
 # функция, которая выдает реферированные тексты + позволяет добавлять параметры
-def summarize_text(request):
+def summarize_text(request, work_program_id):
     summary = None
     description_text = None
     if request.method == 'POST':
@@ -112,75 +118,94 @@ def summarize_text(request):
             max_length_from_user = form.cleaned_data['max_length']
             special_token = form.cleaned_data['special_token']
             truncation_from_user = form.cleaned_data['truncation']
+
+            # Применяем модель для суммаризации текста
+            model_name = "IlyaGusev/rut5_base_headline_gen_telegram"
+            tokenizer = AutoTokenizer.from_pretrained(model_name)
+            model = T5ForConditionalGeneration.from_pretrained(model_name)
+
+            #гиперпараметры
+            max_length= max_length_from_user if max_length_from_user else 600
+            add_special_tokens = True if special_token == 'on' else False
+            truncation = True if truncation_from_user == 'on' else False
             
-            # Получаем ID описания из формы
-            description_id = form.cleaned_data['description_id']
-
+            #достаем описание 
             try:
-                #проверяем, есть ли в базе реферированный текст
-                sum_description = Summary_text.objects.get(wp_id=description_id)
-                summary = sum_description.summirize_text
-            except Summary_text.DoesNotExist:
-                # Применяем модель для суммаризации текста
-                model_name = "IlyaGusev/rut5_base_headline_gen_telegram"
-                tokenizer = AutoTokenizer.from_pretrained(model_name)
-                model = T5ForConditionalGeneration.from_pretrained(model_name)
+                description = Description.objects.get(work_program_id=work_program_id)
+                description_text = description.description_text
+            except Description.DoesNotExist:
+                return render(request, 'summarization.html', {'form': form,'error_msg': 'Описания не существует'})
 
-                #гиперпараметры
-                max_length= max_length_from_user if max_length_from_user else 600
-                add_special_tokens = True if special_token else False
-                truncation = True if truncation_from_user else False
-                
-                #достаем описание 
-                try:
-                    description = Description.objects.get(work_program_id=description_id)
-                    description_text = description.description_text
-                except Description.DoesNotExist:
-                    return render(request, 'summarization.html', {'form': form,'error_msg': 'Описания не существует'})
+            input_ids = tokenizer(
+                [description_text],
+                max_length=max_length,
+                add_special_tokens=add_special_tokens,
+                padding="max_length",
+                truncation=truncation,
+                return_tensors="pt"
+            )["input_ids"]
 
-                input_ids = tokenizer(
-                    [description_text],
-                    max_length=max_length,
-                    add_special_tokens=add_special_tokens,
-                    padding="max_length",
-                    truncation=truncation,
-                    return_tensors="pt"
-                )["input_ids"]
+            output_ids = model.generate(
+                input_ids=input_ids
+            )[0]
 
-                output_ids = model.generate(
-                    input_ids=input_ids
-                )[0]
+            summary = tokenizer.decode(output_ids, skip_special_tokens=True)
 
-                summary = tokenizer.decode(output_ids, skip_special_tokens=True)
-
-                # Создаем и сохраняем объект Summary_text в базу данных
-                summary_object = Summary_text(summarize_text=summary, wp_id = description)
-                summary_object.save()
+            # Создаем и сохраняем объект Summary_text в базу данных
+            summary_object = Summary_text(summarize_text=summary, wp_id = description)
+            summary_object.save()
+            return redirect('/summarize/') 
 
     else:
         form = SummarizationForm()
 
     return render(request, 'summarization.html', {'form': form,'summary': summary})
 
+#функция, которая выводит все summary тексты
+class SummaryList(ListView):
+    model = Summary_text
+    template_name = 'list_summaries.html'
+
 #функция, которая обновляет аннотированный текст
 class UpdateSummary(UpdateView):
     model = Summary_text
     fields = ['summarize_text']
     template_name = 'update_summary.html'
-    success_url = '/main'
+    success_url = '/summarize/'
 
 # функция, которая удаляет аннотированный текст
 class DeleteSummary(DeleteView):
     model = Summary_text
     template_name = 'del_summary.html'
-    success_url = '/main'
+    success_url = '/summarize/'
 
-# функция, которая добавляет оценку тексту
-class RatingCreateView(CreateView):
-    model = Rating
-    fields = ['summarization_id', 'rating_score', 'comment_text', 'author']
-    template_name = 'create_comment.html'
-    success_url = '/main'
+# функция, которая добаляет оценку тексту
+def create_rating(request, summarization_id):
+    if request.method == 'POST':
+        rating_score = True if request.POST.get('rating_score') == 'on' else False 
+        comment_text = request.POST.get('comment_text')
+        author = request.POST.get('author')
+
+        # Создание объекта рейтинга и сохранение его в базе данных
+        Rating.objects.create(
+            summarization_id=Summary_text.objects.get(summarization_id=summarization_id),
+            rating_score=rating_score,
+            comment_text=comment_text,
+            author=author
+        )
+
+        # После успешного создания рейтинга, перенаправляем пользователя на указанную страницу
+        return redirect('/rating/')
+
+    else:
+        form = RatingForm()
+        # Если метод запроса не POST, отобразите форму создания рейтинга
+        return render(request, 'create_comment.html', {'form': form})
+
+#функция, которая выводит все поставленные оценки
+class SummaryList(ListView):
+    model = Summary_text
+    template_name = 'list_summaries.html'
 
 # функция, которая позволяет просматривать редакторов описания
 def program_editors(request, work_program_id):
@@ -193,7 +218,10 @@ def program_editors(request, work_program_id):
     # Отображаем страницу с информацией о редакторах программы
     return render(request, 'editors.html', {'editors': editors})
 
-
+#функция, которая выводит все поставленные оценки
+class RatingList(ListView):
+    model = Rating 
+    template_name = 'list_ratings.html'
 
 # функция, которая изменяет описание, если оно изменилось по API, а если нет, то оставляет таким же
 # функция, которая заменяет оригинальный текст описания на реферированный текст
