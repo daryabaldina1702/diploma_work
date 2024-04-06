569aa2434680e8b8b698d9b59fcd913291740728
daryabaldina1702 <ddbaldina@mail.ru>
2024-04-07 01:36:25 +0300

    Добавлены templates с bootstrap, исправлены и добавлены view, urls, settings

--- summarization/summarization/settings.py
+++ summarization/summarization/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'summarize_app'
+    'summarize_app',
+    'crispy_forms',
+    'crispy_bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
